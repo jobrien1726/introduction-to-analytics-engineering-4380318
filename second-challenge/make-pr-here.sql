31ede74f6ccc9785be7e4f816507eafd48bc350c
Julia O'Brien <jobrien1726@gmail.com>
2024-07-16 17:51:39 -0400

    added documentation

--- second-challenge/make-pr-here.sql
+++ second-challenge/make-pr-here.sql
@@ -1,12 +1,14 @@
+-- create a product performance model that provides summary metrics for each product offered by the company
+-- in order to more quickly identify which products are selling well, and where, as well as which should be improved
 create view product_perf as
     select
-        p.Product_Id as Product_ID
-        , p.ProductName as Product_Name
-        , p.Category as Product_Category
-        , p.Price as Product_Price
-        , sum(o.Quantity) as Total_Product_Sold
-        , count(o.Order_Id) as Total_Product_Orders
-        , count(distinct c.state) as Total_States
+        p.Product_Id as Product_ID                     -- unique identifier for the product
+        , p.ProductName as Product_Name                -- name of the product, which includes the flavor + whether its a bottle or case
+        , p.Category as Product_Category               -- defines whether it is a bottle or case of olive oil
+        , p.Price as Product_Price                     -- the price of the product
+        , sum(o.Quantity) as Total_Product_Sold        -- the total number of products sold
+        , count(o.Order_Id) as Total_Product_Orders    -- the total number of orders placed
+        , count(distinct c.state) as Total_States      -- number of states orders have been received from
 
 from products p
 join orders o
